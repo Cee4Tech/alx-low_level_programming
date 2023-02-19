75aa5f830b03bf91a892f0b4c81d8fd37875f936
Cee4T4ch <info.cee001@gmail.com>
2023-02-19 04:40:37 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Point the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'Z'; letter++)
+
+	putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
