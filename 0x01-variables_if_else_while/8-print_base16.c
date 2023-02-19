4e8e0fe55a1d246a394f2446745a6e7e6755c465
Cee4T4ch <info.cee001@gmail.com>
2023-02-19 06:44:14 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio>
+
+/**
+ * main - Prints all numbers of base 16 in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+	putchar((nun % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar('\n');
+
+	return (0);
+
+}
