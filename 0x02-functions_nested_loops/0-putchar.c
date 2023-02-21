f04e97ae1fecad07dc925182b496f568cca556f1
Cee4T4ch <info.cee001@gmail.com>
2023-02-21 07:00:21 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,21 @@
-#include <stdio.h>
+#include <main.c>
 
 /**
- * main - Prints _putchar, followed by a new line.
+ * main - main function
+ * _putchar - prints the alphabets putchar
  *
- * Return: 0.
+ * Return: Always 0.
  */
 int main(void)
 {
-	printf
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	putchar ('\n');
+	return (0);
 }
