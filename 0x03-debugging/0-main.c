397248f0541f7179c9e1b295e5415991f9cd31c8
Cee4T4ch <info.cee001@gmail.com>
2023-02-23 06:59:12 -0800

    0-main.c, main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints integer is possitive or negative
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int i;
+
+	i = 0;
+	positive_ornegative(i);
+
+	return (0);
+}
