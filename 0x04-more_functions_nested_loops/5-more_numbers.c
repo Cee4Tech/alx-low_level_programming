5c7a5e3215328f903857b9981990d5e8923b3620
Cee4T4ch <info.cee001@gmail.com>
2023-02-23 16:04:33 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers to 14
+ *
+ * Return: returns nothing
+ */
+
+void more_numbers(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 < 10; n1++)
+	{
+		for (n2 = 0; n2 <= 14; n2++)
+		{
+			if (n2 > 9)
+			{
+				putchar((n2 / 10) + '0');
+			}
+			putchar((n2 % 10) + '0');
+		}
+		putchar(10);
+	}
+}
+
+TASK 6.
+
+#include "main.h"
+
+/**
+ * print_line - print n underscore characters
+ * @n: number of times to print _
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
